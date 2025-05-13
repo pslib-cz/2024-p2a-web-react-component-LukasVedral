d71426f4c0f533742dc1f300c641d9848dffb933
Lukas Vedral <lukas.vedral.023@pslib.cz>
2025-05-13 13:34:26 +0200

    range

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css'
 import ViewBox  from './components/PercentView'
-
+import { useState } from 'react'
+/*<ViewBox value={50} max={100} makeColor={(p) => `hsl(${p*100},100%,50%)`}/>
+<ViewBox value={25} max={100} makeColor={(p) => `hsl(${p*100},100%,50%)`}/>*/
 
 
 const App = () => {
+  //dekompozice(misto [0], [1] muzu pouzit promenny)
+  const [rangeValue, setRangeValue] = useState(100);
+  const initial:number = 50;
+
+
+
+
   return ( 
     <> 
-      <ViewBox value={50} max={100} makeColor={(p) => `hsl(100,100%,${p*50}%)`}/>
-      <ViewBox value={25} max={100} makeColor={(p) => `hsl(100,100%,${p*50}%)`}/>
-      <ViewBox value={75} max={100} makeColor={(p) => `hsl(100,100%,${p*50}%)`}/>
+    <div>
+      <input type="range" min={0} max={100} onChange={(e) => {setRangeValue(parseInt(e.target.value))}} defaultValue={initial}/>
+    </div>
+    {console.log(rangeValue)}
+      <ViewBox value={rangeValue} max={100} makeColor={(p) => `hsl(${p*100},100%,50%)`}/>
     </>
   )
 }
